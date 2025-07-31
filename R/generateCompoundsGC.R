f4524a27f719a246f372b6b9ed6fd2edc1bb9f15
Theodora-lhy <theodoraworkmail27@gmail.com>
2025-07-31 09:17:32 +0800

    Update generateCompoundsGC.R

--- R/generateCompoundsGC.R
+++ R/generateCompoundsGC.R
@@ -50,12 +50,13 @@ setMethod("generateCompoundsGC", "featureGroups", function(fGroups, MSPeakLists,
   gCount <- length(fGroups)
   gInfo <- as.data.table(groupInfo(fGroups))
 
-# Defensive check: rename 'retention time' column if it's not already 'ret'
+# Check if retention time exists under any known name
 if (!"ret" %in% names(gInfo)) {
-  rtCol <- grep("^ret(ention)?", names(gInfo), value = TRUE, ignore.case = TRUE)
+  rtCol <- grep("^ret(ention)?|^rt$", names(gInfo), value = TRUE, ignore.case = TRUE)
   if (length(rtCol) == 1) setnames(gInfo, rtCol, "ret")
   else stop("Retention time column not found in feature group info (needed to compute RI)")
 }
+
  
   annTbl <- annotations(fGroups)
   libRecs <- records(MSLibrary)
